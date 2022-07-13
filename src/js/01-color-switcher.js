0dd47032c307c1751a15334ae0e40143022b68be
kate-zahorui <kate.zahorui@gmail.com>
2022-07-13 11:02:22 +0300

    Task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyRef = document.body;
+const startButtonRef = document.querySelector('button[data-start]');
+const stopButtonRef = document.querySelector('button[data-stop]');
+
+let intervalId = null;
+let isActive = false;
+
+const onSwitcherStart = function () {
+  if (isActive) {
+    return;
+  }
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  isActive = true;
+};
+
+const onSwitcherStop = function () {
+  clearInterval(intervalId);
+  isActive = false;
+};
+
+startButtonRef.addEventListener('click', onSwitcherStart);
+stopButtonRef.addEventListener('click', onSwitcherStop);
