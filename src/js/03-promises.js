6c9ea2185ce605e4a8fbde48debb1ae89a0979ff
kate-zahorui <kate.zahorui@gmail.com>
2022-07-14 18:21:46 +0300

    Task 03 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+
+let position = 0;
+let intervalId = null;
+
+function createPromise(position, totalDelay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, totalDelay });
+      } else {
+        reject({ position, totalDelay });
+      }
+    }, totalDelay);
+  });
 }
+
+const onFormSubmit = function (event) {
+  event.preventDefault();
+  const { delay, step, amount } = formRef.elements;
+
+  let totalDelay = Number(delay.value);
+
+  intervalId = setInterval(() => {
+    position += 1;
+    createPromise(position, totalDelay)
+      .then(({ position, totalDelay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${totalDelay}ms`
+        );
+      })
+      .catch(({ position, totalDelay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${totalDelay}ms`
+        );
+      });
+
+    totalDelay += Number(step.value);
+
+    if (position === Number(amount.value)) {
+      clearInterval(intervalId);
+      return;
+    }
+  }, 0);
+};
+
+formRef.addEventListener('submit', onFormSubmit);
